e27b6b9e704224263ce581aabfcfca486fb8d821
Todd Hooper <todd@collectiveip.com>
2013-11-17 23:09:49 -0700

    added a test and incremented version

--- test/lib/prerender_rails.rb
+++ test/lib/prerender_rails.rb
@@ -13,6 +13,7 @@ describe Rack::Prerender do
     @prerender = Rack::Prerender.new(@app)
   end
 
+
   it "should return a prerendered response for a crawler with the returned status code and headers" do
     request = Rack::MockRequest.env_for "/", "HTTP_USER_AGENT" => bot
     stub_request(:get, @prerender.build_api_url(request)).with(:headers => { 'User-Agent' => bot }).to_return(:body => "<html></html>", :status => 301, :headers => { 'Location' => 'http://google.com'})
@@ -23,6 +24,7 @@ describe Rack::Prerender do
     assert_equal( { 'location' => 'http://google.com', 'Content-Length' => '13'}, response[2].headers )
   end
 
+
   it "should return a prerendered reponse if user is a bot by checking for _escaped_fragment_" do
     request = Rack::MockRequest.env_for "/path?_escaped_fragment_=", "HTTP_USER_AGENT" => user
     stub_request(:get, @prerender.build_api_url(request)).with(:headers => { 'User-Agent' => user }).to_return(:body => "<html></html>")
@@ -31,12 +33,14 @@ describe Rack::Prerender do
     assert_equal ["<html></html>"], response[2].body
   end
 
+
   it "should continue to app routes if the url is a bad url with _escaped_fragment_" do
     request = Rack::MockRequest.env_for "/path?query=string?_escaped_fragment_=", "HTTP_USER_AGENT" => user
     response = Rack::Prerender.new(@app).call(request)
 
     assert_equal "", response[2]
   end
+
 
   it "should continue to app routes if the request is not a GET" do
     request = Rack::MockRequest.env_for "/path?_escaped_fragment_=", { "HTTP_USER_AGENT" => user, "REQUEST_METHOD" => "POST" }
@@ -45,12 +49,14 @@ describe Rack::Prerender do
     assert_equal "", response[2]
   end
 
+
   it "should continue to app routes if user is not a bot by checking agent string" do
     request = Rack::MockRequest.env_for "/", "HTTP_USER_AGENT" => user
     response = Rack::Prerender.new(@app).call(request)
 
     assert_equal "", response[2]
   end
+
 
   it "should continue to app routes if user is a bot, but the bot is requesting a resource file" do
     request = Rack::MockRequest.env_for "/main.js?anyQueryParam=true", "HTTP_USER_AGENT" => bot
@@ -59,12 +65,14 @@ describe Rack::Prerender do
     assert_equal "", response[2]
   end
 
+
   it "should continue to app routes if the url is not part of the regex specific whitelist" do
     request = Rack::MockRequest.env_for "/saved/search/blah", "HTTP_USER_AGENT" => bot
     response = Rack::Prerender.new(@app, whitelist: ['^/search', '/help']).call(request)
 
     assert_equal "", response[2]
   end
+
 
   it "should return a prerendered response if the url is part of the regex specific whitelist" do
     request = Rack::MockRequest.env_for "/search/things/123/page", "HTTP_USER_AGENT" => bot
@@ -74,12 +82,14 @@ describe Rack::Prerender do
     assert_equal ["<html></html>"], response[2].body
   end
 
+
   it "should continue to app routes if the url is part of the regex specific blacklist" do
     request = Rack::MockRequest.env_for "/search/things/123/page", "HTTP_USER_AGENT" => bot
     response = Rack::Prerender.new(@app, blacklist: ['^/search', '/help']).call(request)
 
     assert_equal "", response[2]
   end
+
 
   it "should return a prerendered response if the url is not part of the regex specific blacklist" do
     request = Rack::MockRequest.env_for "/profile/search/blah", "HTTP_USER_AGENT" => bot
@@ -89,12 +99,14 @@ describe Rack::Prerender do
     assert_equal ["<html></html>"], response[2].body
   end
 
+
   it "should continue to app routes if the referer is part of the regex specific blacklist" do
     request = Rack::MockRequest.env_for "/api/results", "HTTP_USER_AGENT" => bot, "HTTP_REFERER" => '/search'
     response = Rack::Prerender.new(@app, blacklist: ['^/search', '/help']).call(request)
 
     assert_equal "", response[2]
   end
+
 
   it "should return a prerendered response if the referer is not part of the regex specific blacklist" do
     request = Rack::MockRequest.env_for "/api/results", "HTTP_USER_AGENT" => bot, "HTTP_REFERER" => '/profile/search'
@@ -103,6 +115,16 @@ describe Rack::Prerender do
 
     assert_equal ["<html></html>"], response[2].body
   end
+
+
+  it "should return a prerendered response returned from before_render" do
+    request = Rack::MockRequest.env_for "/", "HTTP_USER_AGENT" => bot
+    stub_request(:get, @prerender.build_api_url(request)).to_return(:body => "<html></html>")
+    response = Rack::Prerender.new(@app, before_render: Proc.new do |env| return '<html>cached</html>' end).call(request)
+
+    assert_equal ["<html>cached</html>"], response[2].body
+  end
+
  
   describe '#buildApiUrl' do
     it "should build the correct api url with the default url" do
@@ -111,6 +133,7 @@ describe Rack::Prerender do
       assert_equal 'http://prerender.herokuapp.com/https://google.com/search?q=javascript', @prerender.build_api_url(request)
     end
 
+
     it "should build the correct api url with an environment variable url" do
       ENV['PRERENDER_SERVICE_URL'] = 'http://prerenderurl.com'
       request = Rack::MockRequest.env_for "https://google.com/search?q=javascript"
@@ -118,11 +141,13 @@ describe Rack::Prerender do
       ENV['PRERENDER_SERVICE_URL'] = nil
     end
 
+
     it "should build the correct api url with an initialization variable url" do
       @prerender = Rack::Prerender.new(@app, prerender_service_url: 'http://prerenderurl.com')
       request = Rack::MockRequest.env_for "https://google.com/search?q=javascript"
       assert_equal 'http://prerenderurl.com/https://google.com/search?q=javascript', @prerender.build_api_url(request)
     end
+
 
     # Check CF-Visitor header in order to Work behind CloudFlare with Flexible SSL (https://support.cloudflare.com/hc/en-us/articles/200170536)
     it "should build the correct api url for the Cloudflare Flexible SSL support" do
