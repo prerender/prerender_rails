26d50faf8130e19ffa290a489771855b0c60cecf
Dave Lerman <david.lerman@1deg.org>
2017-04-13 10:52:29 -0700

    Add Qwantify to crawler list
    Search engine is https://www.qwant.com/. User agent is
    Mozilla/5.0 (compatible; Qwantify/2.3w; +https://www.qwant.com/)/2.3w

--- lib/prerender_rails.rb
+++ lib/prerender_rails.rb
@@ -28,7 +28,8 @@ module Rack
         'vkShare',
         'W3C_Validator',
         'redditbot',
-        'Applebot'
+        'Applebot',
+        'Qwantify'
       ]
 
       @extensions_to_ignore = [
